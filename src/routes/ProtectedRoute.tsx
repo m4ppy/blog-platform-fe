10a408380c4e30508fb73a96f6f8a5a5874edfc5
m4ppy <leon.choi.dev@gmail.com>
2025-12-29 16:34:06 +0900

    adjust AuthContext to route system

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,8 +1,28 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "../auth/AuthContext";
+
 type Props = {
   children: React.ReactNode;
 };
 
 function ProtectedRoute({ children }: Props) {
+  const authContext = useContext(AuthContext);
+
+  if (!authContext) {
+    throw new Error("AuthContext not found");
+  }
+
+  const { auth, initialized } = authContext;
+
+  if (!initialized) {
+    return null;
+  }
+
+  if (!auth.accessToken) {
+    return <Navigate to="/login" replace />;
+  }
+
   return children;
 }
 
