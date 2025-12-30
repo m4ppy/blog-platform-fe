a708589bcce4e644e874139a34dea417a84de1cf
m4ppy <leon.choi.dev@gmail.com>
2025-12-30 10:51:17 +0900

    fix navbar UI

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Group, Title, Button, Container } from "@mantine/core";
+import { Group, Title, Button, Container, Anchor } from "@mantine/core";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../auth/AuthContext";
@@ -12,7 +12,22 @@ export default function Navbar() {
     <Container size="lg" h="100%">
       <Group justify="space-between" h="100%" bg="gray">
         {/* Logo / Title */}
-        <Title order={3}>Blog Platform</Title>
+        <Title order={3}>
+          <Anchor component={Link} to="/" underline="never" c="inherit">
+            Blog Platform
+          </Anchor>
+        </Title>
+
+        {/* Center: Navigation tabs */}
+        <Group gap="lg">
+          <>
+            <Anchor component={Link} to="/posts">Posts</Anchor>
+            <Anchor component={Link} to="/tags">Tags</Anchor>
+            {isLoggedIn && (
+              <Anchor component={Link} to="/drafts">Drafts</Anchor>
+            )}
+          </>
+        </Group>
 
         {/* Right side */}
         <Group>
